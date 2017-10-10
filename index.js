a25d2fa413f25c43927e74d9e060c806c8b8f46a
Asher Shaheen <ashershaheen@gmail.com>
2017-10-10 12:58:08 -0400

    added polyline on the map by user location

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 console.log('JS is loaded');
 
+var runData = [];
+var poly;
+var latLngBounds;
+
 function initMap () {
   navigator.geolocation.watchPosition((pos) => {
     const { latitude: lat, longitude: lng } = pos.coords;
-    console.log('new position--->', pos.coords)
+    runData.push(new google.maps.LatLng(lat, lng))
+    console.log('new position--->', runData)
 
     map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat, lng },
-      zoom: 16
-    }),
+      center: runData[0],
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    latLngBounds = new google.maps.LatLngBounds();
+    for (let i = 0; i < runData.length; i++) {
+      latLngBounds.extend(runData[i]);
+      new google.maps.Marker({
+        map: map,
+        position: runData[i],
+        title: "Point " + (i + 1)
+      });
+    }
+
+    poly = new google.maps.Polyline({
+      strokeColor: '#000000',
+      strokeOpacity: 1.0,
+      strokeWeight: 3,
+      path: runData,
+      map: map
+    });
+    
+    map.fitBounds(latLngBounds);
+
 
     marker = new google.maps.Marker({
       position: { lat, lng },
@@ -18,6 +45,11 @@ function initMap () {
   }, err => {
     console.error(err)
   }, options)
+}
+
+function addLatLng (event) {
+  var path = poly.getPath()
+  console.log(path)
 }
 
 
