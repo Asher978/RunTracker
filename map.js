556a5ac185c69850a51850f67ee4a4bda5e9dcee
Asher Shaheen <ashershaheen@gmail.com>
2017-10-10 16:55:41 -0400

    changed CDN

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
 console.log('map.js is loaded!');
 var map;
+var mapId = 'mapbox.run-bike-hike';
+var access_token = 'pk.eyJ1IjoiYXNoZXI5NzgiLCJhIjoiY2o1eTVmNXlnMGJ2NjJ5cWRxMTRtY2hsMSJ9.y7O2ehEprrX26JpPyZatrQ';
 var runData = [];
 let startBtn = document.getElementById('start');
 startBtn.addEventListener('click', () => startRun())
@@ -22,14 +24,30 @@ function startRun () {
     const { latitude: lat, longitude: lng } = pos.coords;
     runData.push(lat, lng)
     console.log(runData)
-    polyline = L.polyline([runData], 
-    {
-      color: 'red',
-      weight: 10,
-      opacity: .7,
-      dashArray: '20,15',
-      lineJoin: 'round'
-    }).addTo(map);
+    polyline = L.polyline([
+      [40.739804199999995, -73.9895718],
+      [40.739804199999995, -74.9895718],
+      [40.739804199999995, -75.9895718]
+      
+      ],
+      {
+          color: 'red',
+          weight: 10,
+          opacity: .7,
+          dashArray: '20,15',
+          lineJoin: 'round'
+      }
+      ).addTo(map);
+
+
+    // polyline = L.polyline([runData], 
+    // {
+    //   color: 'red',
+    //   weight: 10,
+    //   opacity: .7,
+    //   dashArray: '20,15',
+    //   lineJoin: 'round'
+    // }).addTo(map);
   })
 }
         
